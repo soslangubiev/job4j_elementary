75e0f8862cb998a5eff30124601e961ef84dbe3f
Soslan Gubiev <Gubievs91@gmail.com>
2022-07-12 06:46:01 +0500

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double result = (height - 100) * 1.15;
+        return result;
+    }
+
+    public static double womanWeight(short height) {
+        double result = (height - 110) * 1.15;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Ideal weight for a man with a height of " + height + " is: " + man);
+        System.out.println("Ideal weight for a woman with a height of " + height + " is: " + woman);
+    }
+}
