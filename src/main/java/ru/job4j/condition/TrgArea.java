b1de0ffcd7e026e869ab6813bce94109a00e6733
Soslan Gubiev <Gubievs91@gmail.com>
2022-07-13 07:40:25 +0500

    9. Площадь треугольника. [#175474 #321205] correction of area method

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,10 +2,8 @@ package ru.job4j.condition;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double result;
         double p = (a + b + c) / 2;
-        result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return result;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
