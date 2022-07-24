5a8c4edf91cd1d44a23c980fc09478e819c47fc2
Soslan Gubiev <Gubievs91@gmail.com>
2022-07-24 22:41:03 +0500

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741]

---
+++ src/test/java/ru/job4j/array/MachineTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static ru.job4j.array.Machine.*;
+
+public class MachineTest {
+
+    @Test
+    public void whenEquals() {
+        int money = 100;
+        int price = 100;
+        int[] result = change(money, price);
+        int[] expected = {};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void when50by35() {
+        int money = 50;
+        int price = 35;
+        int[] result = change(money, price);
+        int[] expected = {10, 5};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void when50by21() {
+        int money = 50;
+        int price = 21;
+        int[] result = change(money, price);
+        int[] expected = {10, 10, 5, 2, 2};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void when50by32() {
+        int money = 50;
+        int price = 32;
+        int[] result = change(money, price);
+        int[] expected = {10, 5, 2, 1};
+        assertArrayEquals(expected, result);
+    }
+}
