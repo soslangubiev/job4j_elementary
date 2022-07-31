a9e4507c792c0733aad6dd2ca1e38cc8692e408d
Soslan Gubiev <Gubievs91@gmail.com>
2022-07-31 06:23:33 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,7 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left > right ? left : right;
+    public static int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        return first > max(second, third) ? first : max(second, third);
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(first, second) > max(third, fourth) ? max(first, second) : max(third, fourth);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,31 +6,51 @@ import static ru.job4j.condition.Max.*;
 import static org.junit.Assert.*;
 
 public class MaxTest {
-
     @Test
-    public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = max(left, right);
+    public void whenMax1And2Then2() {
+        int first = 1;
+        int second = 2;
+        int result = max(first, second);
         int expected = 2;
         assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax10To5Then10() {
-        int left = 10;
-        int right = 5;
-        int result = max(left, right);
+    public void whenMax10And5Then10() {
+        int first = 10;
+        int second = 5;
+        int result = max(first, second);
         int expected = 10;
         assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax22To22Then22() {
-        int left = 22;
-        int right = 22;
-        int result = max(left, right);
+    public void whenMax22And22Then22() {
+        int first = 22;
+        int second = 22;
+        int result = max(first, second);
         int expected = 22;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax22And42And56Then56() {
+        int first = 22;
+        int second = 42;
+        int third = 56;
+        int result = max(first, second, third);
+        int expected = 56;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax88And42And56And44Then22() {
+        int first = 88;
+        int second = 42;
+        int third = 56;
+        int fourth = 44;
+        int result = max(first, second, third, fourth);
+        int expected = 88;
         assertEquals(result, expected);
     }
 }
